4bf4daf9cb0cf15b8ba5fe3fa6bbd8ba9afd75a9
Limit7 <Limit7@limit7.vip>
2021-02-24 17:23:28 +0800

    first

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: '开发手册',
+    description: '练习文档',
+    base: "/",
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/img/favicon.ico`
+        }]
+    ],
+    theme: 'antdocs'
+}
