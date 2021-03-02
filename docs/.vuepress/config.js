3346d03652d58639b18cb0e66af56c6c0a1ac7c6
Limit7 <Limit7@limit7.vip>
2021-03-02 18:11:16 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,25 @@
+//https://segmentfault.com/a/1190000017055963
+
 module.exports = {
     title: '开发手册',
     description: '练习文档',
-    base: "/",
+    //base: "/",
     head: [
         ['link', {
             rel: 'icon',
             href: `/img/favicon.ico`
         }]
     ],
-    theme: 'antdocs'
+    theme: 'antdocs',
+    themeConfig: {
+        //sidebar:'auto'
+    },
+    plugins: ['@vuepress/active-header-links', {
+        sidebarLinkSelector: '.sidebar-link',
+        headerAnchorSelector: '.header-anchor'
+
+    },
+        'vuepress-plugin-auto-sidebar', {}
+    ]
+
 }
